707de9c8700dbffa742be2c978adb9482b3f20ef
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-25 10:04:25 -0300

    Init eco

---
+++ Markiw/1-init.py
@@ -0,0 +1,7 @@
+#Os Deis princípios
+#Comos as Pessoas tomam decisões
+#Não há segredo, uma economia é o resultado do amalgama de desisões individuais
+
+# 1 - Tradeoffs
+# 1.1 - Nada é de graça
+# 1.2 - Cada escolha uma renúncia
---
+++ Markiw/init.py
@@ -0,0 +1,13 @@
+
+
+#37/886
+#file:///L:/Books/Economia/Introdu%C3%A7%C3%A3o%20%C3%A0%20Economia%20-%20Mankiw.pdf
+
+
+familia = [
+    'Pessoas', 'casa', 'administrar', 'recursos', 'escassos',
+]
+
+sociedade = [
+    'Pessoas', 'Produtos', 'Serviços', 
+]
